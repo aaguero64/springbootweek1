8b2b35dafdd4a7ade717cf3abeca310660940038
aaguero64 <71055422+aaguero64@users.noreply.github.com>
2020-12-19 23:22:13 -0800

    Add files via upload

---
+++ App.java
@@ -0,0 +1,15 @@
+package com.sierraspringboot.mvnProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("com.sierraspringboot.mvnProject")
+@SpringBootApplication
+public class App 
+{
+    public static void main(String[] args )
+    {
+       SpringApplication.run(App.class, args);
+    }
+}
---
+++ CarController.java
@@ -0,0 +1,30 @@
+package com.sierraspringboot.mvnProject.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CarController {
+	
+	 
+	List<String> cars = new ArrayList<String>();
+		
+	@RequestMapping(value="/cars", method=RequestMethod.POST)
+	public String addCar(@RequestBody String car)
+	
+	{
+		cars.add(car);
+		return "Successfully Car add: " + car;
+	}
+	
+	@RequestMapping ("/cars")
+	public List<String> getCars(){
+		return cars;
+	}
+
+}
